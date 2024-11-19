46154f2e34581c4755fc62eaf81000c599b1e1c9
milk <milk00000000000000@gmail.com>
2024-11-19 17:47:18 +0100

    add logic for refresh

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -6,11 +6,17 @@ let interval = null;
 
 //MAKE A REFRESH
 router.route('/refresh').get(async (req, res) => {
+  console.group('Request data:');
+  console.log('Request for refresh was at ', new Date().toISOString());
+
+
   try {
     interval = setTimeout(async () => {
-      // await fetch('https://fuel-price-r6f2.onrender.com/api/v1/refresh');
-      await fetch('http://localhost:3000/api/v1/refresh');
-      console.log('https://fuel-price-r6f2.onrender.com/api/v1/refresh refreshed at ', new Date().toISOString());
+      await fetch('https://fuel-price-r6f2.onrender.com/api/v1/refresh');
+
+      console.log('Request back was at ', new Date().toISOString());
+
+      console.groupEnd();
     }, 1000*60*5);
 
     res.status(201).json({success: true, message: 'Success! fuel-price-r6f2.onrender.com will be updated after 5 min'});
