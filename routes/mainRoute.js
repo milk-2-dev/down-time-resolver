08cd1020cd06327991d16fbcdbe3ab72dbbf176e
milk <milk00000000000000@gmail.com>
2024-12-16 16:29:53 +0100

    downtime resolver

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -2,32 +2,35 @@ import express from 'express';
 
 const router = express.Router();
 
-let interval = null;
+let timer = null;
 
 //MAKE A REFRESH
 router.route('/refresh').get(async (req, res) => {
+  if (timer) {
+    clearTimeout(timer);
+  }
+
   console.group('Request data:');
-  console.log('Request from api was at ', new Date().toISOString());
+  console.log('Request from api - ', new Date().toISOString());
 
   try {
-    interval = setTimeout(async () => {
+    timer = setTimeout(async () => {
       await Promise.all([
-        await fetch('https://fuel-price-r6f2.onrender.com/api/v1/refresh'),
-        await fetch('https://cfm-fuelprices.onrender.com/api/v1/refresh')
+        fetch('https://fuel-price-r6f2.onrender.com/api/v1/refresh'),
+        fetch('https://cfm-fuelprices.onrender.com/api/v1/refresh')
       ]);
 
-      console.log('Request to api was at ', new Date().toISOString());
-
-    }, 1000*60*5);
+      console.log('Request to api - ', new Date().toISOString());
+    }, 1000 * 60 * 5);
 
     res.status(201).json({success: true, message: 'Success! API will be updated after 5 min'});
   } catch (error) {
     console.log('Something went wrong: ', error);
-    clearTimeout(interval);
+    clearTimeout(timer);
     res.status(500).json({success: false, message: error});
-  } finally {
-    console.groupEnd();
   }
+
+  console.groupEnd();
 });
 
 export default router;
