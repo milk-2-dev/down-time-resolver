e376ad38f02bd5ffe77bb4f68d0445baf1e56f0c
milk <milk00000000000000@gmail.com>
2024-12-16 13:51:26 +0100

    add some logs

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -7,8 +7,7 @@ let interval = null;
 //MAKE A REFRESH
 router.route('/refresh').get(async (req, res) => {
   console.group('Request data:');
-  console.log('Request for refresh was at ', new Date().toISOString());
-
+  console.log('Request from api was at ', new Date().toISOString());
 
   try {
     interval = setTimeout(async () => {
@@ -17,15 +16,17 @@ router.route('/refresh').get(async (req, res) => {
         await fetch('https://cfm-fuelprices.onrender.com/api/v1/refresh')
       ]);
 
-      console.log('Request back was at ', new Date().toISOString());
+      console.log('Request to api was at ', new Date().toISOString());
 
-      console.groupEnd();
     }, 1000*60*5);
 
-    res.status(201).json({success: true, message: 'Success! fuel-price-r6f2.onrender.com will be updated after 5 min'});
+    res.status(201).json({success: true, message: 'Success! API will be updated after 5 min'});
   } catch (error) {
+    console.log('Something went wrong: ', error);
     clearTimeout(interval);
     res.status(500).json({success: false, message: error});
+  } finally {
+    console.groupEnd();
   }
 });
 
